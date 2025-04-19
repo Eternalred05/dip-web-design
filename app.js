69c51a763d12cce28979075e740e37ba62ae0dd3
Brian <mbrianp05@gmail.com>
2025-04-19 16:44:07 -0400

    Remove header carrousel & add some more content

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const yearPickerLinks = document.querySelectorAll("#year-picker a");
+const timelineTitle = document.querySelector("#timeline-title");
+const header = document.querySelector("#hero header");
+const wrapper = document.querySelector("#timeline-wrapper");
 
-yearPickerLinks.forEach((link) => {
-  link.addEventListener("click", () => changeActive(link));
-});
+let up = false;
 
 function changeActive(link) {
   yearPickerLinks.forEach((l) => l.parentElement.classList.remove("active"));
 
   link.parentElement.classList.add("active");
 }
+
+wrapper.addEventListener("scroll", () => {
+  const currentYear = Math.floor(wrapper.scrollTop / wrapper.clientHeight);
+
+  changeActive(yearPickerLinks[currentYear]);
+});
+
+window.addEventListener("scroll", () => {
+  if (up) {
+    timelineTitle.style.translate = "0 0";
+  }
+
+  if (timelineTitle.getBoundingClientRect().top < header.clientHeight) {
+    timelineTitle.style.translate = "0 -75%";
+    up = true;
+  }
+});
